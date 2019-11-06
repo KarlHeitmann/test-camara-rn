b9e2526d6f00f47ce1c8a49ffa6c7689419a5568
Paulo Granados <paulogranados089@gmail.com>
2019-11-06 19:00:35 -0300

    funciono la refactorizacion

--- App.js
+++ App.js
@@ -7,91 +7,15 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TouchableHighlight,
-  Image
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Pagina from "./src/pagina.js"
 
-class Pagina extends React.Component {
-  _onPressButton = () => {
-    console.log("Hola")
-  } 
+class App extends React.Component{
   render(){
-    const titulo = "Max Qlo pesao"
     return(
-      <View>
-        <Text>
-          {titulo}
-        </Text>
-        <TouchableHighlight onPress={this._onPressButton}>
-          <Image
-            style={styles.button}
-            source={require('./mybutton.png')}
-          />
-        </TouchableHighlight>
-      </View>
-
-    )
+      <Pagina/>
+      )
   }
 }
 
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10
-  },
-});
-
-export default Pagina;
+export default App;
